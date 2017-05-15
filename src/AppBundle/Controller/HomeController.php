35c7e3532a560b0531e112f05578b12b3477a7b4
4LB3RT <albertgarcia.agc@gmail.com>
2017-05-15 20:30:55 +0200

    Formulario de registro pasado a la vista

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class HomeController extends Controller
 {
@@ -13,7 +16,32 @@ class HomeController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $form = $this->new_user();
+
         // replace this example code with whatever you need
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
+
+    public function new_user(Request $request){
+
+        //instanciamos un objeto de tipo usuario
+        $user = new User();
+
+        //nos basamos en el objeto usuario para crear el formulario de registro de usuarios
+        $form = $this->createFormBuilder($user)
+            ->add('nombrecompleto', TextType::class)
+            ->add('username', TextType::class)
+            ->add('contrasenya', TextType::class)
+            ->add('email', TextType::class)
+            ->add('ciudad', TextType::class)
+            ->add('imagen', TextType::class)
+            ->add('sexo', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Registrate'))
+            ->getForm();
+
+        return $form;
+    }
+
 }
