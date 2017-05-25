2cc141121c883cb81967d4b8f3d9afce0c7bb8bb
Unknown <gerard.c.tw@gmail.com>
2017-05-25 17:13:12 +0200

    WIP - home

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -5,7 +5,37 @@ $( document ).ready(function() {
         var heightf = $(".footer-home").height();
         var heightw = $( document ).height() - (heighth + heightf);
         $(".content").css("height",heightw+"px");
+    });
+
+/*
+    $(".homepage .password-cont label" ).each(function (){
+        console.log($(this));
+        if( $(this).text() == "Plain password")
+        {
+
+            $(this).css("display","none");
+        }
     })
+*/
+
+
+    $('.homepage form div input').on('focus', function() {
+        if ($(this).val() === "") {
+            $(this).prev('label').animate({
+                fontSize: 13,
+                top: -25
+            }, 350);
+        }
+
+    });
+    $('.homepage form div input').on('blur', function() {
+        if ($(this).val() === "") {
+            $(this).prev('label').animate({
+                fontSize: 17,
+                top: 0
+            }, 350);
+        }
+    });
 
 
 });
