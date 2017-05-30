8a10f64ec4eaada70a70679e1616a58aa2324886
Unknown <gerard.c.tw@gmail.com>
2017-05-30 20:27:13 +0200

    WIP - Responsive home, registro

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,11 +1,14 @@
 $( document ).ready(function() {
-    $(".homepage, .register").ready(function () {
         var heighth = $(".home-head").height();
         var heightf = $(".footer-home").height();
-        var heightw = $( document ).height() - (heighth + heightf);
+
+        if(heightf!=0){
+            var heightw = $( window ).height() - (heighth);
+        }else{
+            var heightw = $( window ).height() - (heighth + heightf);
+        }
         $(".content").css("height",heightw+"px");
         $(".wrapper-register").css("height",heightw+"px");
-    });
 
 
 /*
@@ -45,12 +48,12 @@ $( document ).ready(function() {
         $("#appbundle_user_provincia").select2({
             placeholder: "Provincia...",
             allowClear:true,
-            width: '50%'
+            width: '75%'
         });
         $("#appbundle_user_sexo").select2({
             placeholder: "Sexo...",
             allowClear:true,
-            width: '50%'
+            width: '75%'
         });
 
 
@@ -70,4 +73,21 @@ $( document ).ready(function() {
     function onBlur(){
         deactivateSearchbox(document.querySelector('.searchbox'));
     }
+
+
+
+
+
+    /*BOTON CONTINUAR-VOLVER REGISTRO MOVIL*/
+    var slideW = $('.wrapper-register').width();
+    $('#siguiente-reg').click(function( e ){
+        e.preventDefault();
+        $('.wrapper-register').animate({scrollLeft: slideW }, 600);
+    });
+
+    $('#b-volver').click(function( e ){
+        e.preventDefault();
+        $('.wrapper-register').animate({scrollLeft: -(slideW) }, 600);
+    });
+
 });
